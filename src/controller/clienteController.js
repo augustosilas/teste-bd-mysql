e4edefe33396785ceb4b8d8a29a4362ac81e836d
Silas Augusto <augusto.fernandes775@gmail.com>
2020-04-09 10:07:56 -0300

    end CRUD empregado

--- src/controller/clienteController.js
+++ src/controller/clienteController.js
@@ -11,15 +11,43 @@ module.exports = {
   },
 
   async create(request, response) {
-    const { PrimeiroNome, SegundoNome } = request.body;
-    console.log(PrimeiroNome, SegundoNome);
+    const { PrimeiroNome, UltimoNome } = request.body;
+
     const sql = "INSERT INTO empregado(PrimeiroNome, UltimoNome) VALUES ?";
-    const value = [[PrimeiroNome, SegundoNome]];
+    const value = [[PrimeiroNome, UltimoNome]];
 
     connection.query(sql, [value], (err, results, fields) => {
       if (err) return console.log(err);
 
       return response.json(results);
+    });
+  },
+
+  async update(request, response) {
+    const { PrimeiroNome, UltimoNome } = request.body;
+    const ID = parseInt(request.params.id);
+
+    const sql =
+      "UPDATE empregado\
+                 SET PrimeiroNome = ?, UltimoNome = ?\
+                 WHERE ID = ?;";
+    const value = [PrimeiroNome, UltimoNome, ID];
+    connection.query(sql, value, (err, results, fields) => {
+      if (err) return console.log(err);
+
+      return response.json({ message: "Updated successfully" });
+    });
+  },
+
+  async delete(request, response) {
+    const id = parseInt(request.params.id);
+
+    const sql = "DELETE FROM empregado where ID = ?";
+
+    connection.query(sql, [id], (err, result, fields) => {
+      if (err) throw err;
+
+      return response.json(fields);
     });
   },
 };
--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,7 @@ const EmpregadoController = require("./controller/clienteController");
 
 router.get("/empregados", EmpregadoController.index);
 router.post("/empregados", EmpregadoController.create);
+router.put("/empregados/:id", EmpregadoController.update);
+router.delete("/empregados/:id", EmpregadoController.delete);
 
 module.exports = router;
